49f16c01ebb5c56d4a5cc85cf6a2b5c9b9b9bb2b
anAstrnt <shinohara.2516300@gmail.com>
2024-08-04 14:37:44 +0900

    💄 Top画面の見た目の軽微な修正

--- app/features/home/top/FamilyCardAria/page.tsx
+++ app/features/home/top/FamilyCardAria/page.tsx
@@ -19,7 +19,16 @@ const Page: React.FC = () => {
   };
 
   return (
-    <Grid sx={{ width: "100%", height: "100vh", overflow: "auto" }}>
+    <Grid
+      sx={{
+        width: "100%",
+        height: "100vh",
+        overflow: "auto",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+      }}
+    >
       <Grid>
         <Typography variant="h4" component="div">
           家族メモ
@@ -27,28 +36,30 @@ const Page: React.FC = () => {
         <Typography variant="h6" component="div">
           様々な場面で書類作成するときのため、家族の情報をメモしておこう。
         </Typography>
+      </Grid>
 
-        <Grid sx={{ display: "flex", alignItems: "center" }}>
-          <FamilyCard />
-          <IconButton
-            sx={{ backgroundColor: "rgba(0,0,0,0.3)" }}
-            onClick={openFamilyCardAddSpace}
-          >
-            <AddIcon />
-          </IconButton>
-        </Grid>
+      <Grid
+        sx={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}
+      >
+        <FamilyCard />
+        <IconButton
+          sx={{ backgroundColor: "rgba(0,0,0,0.3)" }}
+          onClick={openFamilyCardAddSpace}
+        >
+          <AddIcon />
+        </IconButton>
+      </Grid>
 
-        {openInputSpace ? <FamilyCardAdd /> : ""}
+      {openInputSpace ? <FamilyCardAdd /> : ""}
 
-        <Grid>
-          <Typography>誕生日</Typography>
-          <Typography>郵便番号</Typography>
-          <Typography>住所</Typography>
-          <Typography>学歴</Typography>
-          <Typography>職歴</Typography>
-          <Typography>資格</Typography>
-          <Typography>メモ</Typography>
-        </Grid>
+      <Grid>
+        <Typography>誕生日</Typography>
+        <Typography>郵便番号</Typography>
+        <Typography>住所</Typography>
+        <Typography>学歴</Typography>
+        <Typography>職歴</Typography>
+        <Typography>資格</Typography>
+        <Typography>メモ</Typography>
       </Grid>
     </Grid>
   );
