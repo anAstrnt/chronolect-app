a444a29650cfc21ea13c3927ef4943399a5c7ade
anAstrnt <shinohara.2516300@gmail.com>
2024-08-08 07:03:24 +0900

    ♻️ FamilyCardをストレージに登録する際に、サブコレクションとしてDetailのデフォルト値も一緒に登録する処理を追記

--- app/components/top/FamilyCardAdd.tsx
+++ app/components/top/FamilyCardAdd.tsx
@@ -3,19 +3,26 @@ import FamilyCardAddButton from "@/components/FamilyCardAddButton";
 import ImageUpload from "@/components/ImageUpload";
 import { db, storage } from "@/libs/firebase";
 import { Grid, TextField, Typography } from "@mui/material";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, doc, getDoc, setDoc } from "firebase/firestore";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const FamilyCardAdd: React.FC = () => {
-  const { userName, setUserName, avatar, setAvatar, setOpenInputSpace } = useFamilyCard();
+  const { userId, userName, setUserName, avatar, setAvatar, setOpenInputSpace } =
+    useFamilyCard();
   // avatarImage：imageUploadコンポーネントからがアバター画像がアップロードされたときにFileデータを格納しておくステート。ファイルからアバター画像をアップするために格納するステート（本ステート）はsendUserのif文で画像をアップロードするために使い、それ以外はアバターのURLのみ格納するステート（avatar,setAvatar）で画像URLを管理している。
   const [avatarImage, setAvatarImage] = useState<File | null>(null);
   const [sampleAvatarImageNum, setSampleAvatarImageNum] = useState<number>();
 
   const sendUser = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    // 新しいFamilyCardを作った際の、DocIDを格納しておくための変数。最後の処理でサブコレクションを追加するために使用する。
+    let newFamilyCardDocRef;
+
+    // 自分で選んだ画像をアバターに登録するときの処理
     if (avatarImage) {
+      // アバター画像名にランダムな文字列を結合し、ストレージに挙げる際に一意のURLになるようにしている。
       const S = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
       const N = 16;
       const randomChar = Array.from(crypto.getRandomValues(new Uint32Array(N)))
@@ -26,25 +33,47 @@ const FamilyCardAdd: React.FC = () => {
       const imageRef = ref(storage, `avatars/${fileName}`);
       await uploadBytes(imageRef, avatarImage);
 
-      getDownloadURL(ref(storage, `avatars/${fileName}`))
-        .then((url) => {
-          addDoc(collection(db, "familyCard"), {
-            avatar: url,
-            userName: userName,
-          });
-        })
-        .catch((err) => err.message);
+      // アバター画像URLとユーザー名をストレージに格納する処理
+      try {
+        const url = await getDownloadURL(ref(storage, `avatars/${fileName}`));
+        newFamilyCardDocRef = await addDoc(collection(db, "familyCard"), {
+          avatar: url,
+          userName: userName,
+        });
+      } catch (error) {
+        console.error(error);
+      }
     }
+
+    // サンプルの画像をアバターに選び登録するときの処理
     if (avatar) {
-      getDownloadURL(ref(storage, `sample_avatar/avatar${sampleAvatarImageNum}.png`))
-        .then((url) => {
-          addDoc(collection(db, "familyCard"), {
-            avatar: url,
-            userName: userName,
-          });
-        })
-        .catch((err) => err.message);
+      // アバター画像URLとユーザー名をストレージに格納する処理
+      try {
+        const url = await getDownloadURL(
+          ref(storage, `sample_avatar/avatar${sampleAvatarImageNum}.png`)
+        );
+        newFamilyCardDocRef = await addDoc(collection(db, "familyCard"), {
+          avatar: url,
+          userName: userName,
+        });
+      } catch (error) {
+        console.error(error);
+      }
     }
+
+    // アバター画像とユーザー名をストレージのコレクションにFamilyCardとして登録する際に、同時にサブコレクションとしてdetailを登録する処理
+    if (newFamilyCardDocRef) {
+      const detailCollectionRef = collection(newFamilyCardDocRef, "detail");
+      await addDoc(detailCollectionRef, {
+        name: "",
+        birthday: "",
+        postCode: "",
+        address: "",
+        qualification: "",
+        email: "",
+      });
+    }
+
     setAvatarImage(null);
     setAvatar("");
     setUserName("");
