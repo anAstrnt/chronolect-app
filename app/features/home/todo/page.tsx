81cf788ec0145037f5fd76969a231c4d62d16e33
anAstrnt <shinohara.2516300@gmail.com>
2024-08-14 12:05:04 +0900

    🚧  Todo機能をつけるためコンポーネントを作成

---
+++ app/features/home/todo/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AddTodoAria from "./AddTodoAria/page";
+import ShowTodoAria from "./ShowTodoAria/page";
+
+const page = () => {
+  return (
+    <>
+      <AddTodoAria />
+      <ShowTodoAria />
+    </>
+  );
+};
+
+export default page;
