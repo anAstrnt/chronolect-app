e83a266a927bbdb8f22ad33c24af75bb94173090
an <shinohara.2516300@gmail.com>
2024-08-15 22:06:49 +0900

    ⚡️ サイドバーからクリックされたページへ遷移するようにしました

--- app/features/home/top/page.tsx
+++ app/features/home/top/page.tsx
@@ -1,13 +1,27 @@
+"use client"
+
 import React from "react";
 import FamilyCardAria from "./FamilyCardAria/page";
 import FamilyCardDetailAria from "./FamilyCardDetailAria/page";
 import { Grid } from "@mui/material";
+import { useRecoilValue } from "recoil";
+import { hasUserDataState } from "@/app/states/hasUserDataState";
+import FirstFamilyCard from "@/app/components/top/FamilyCardAria/FirstFamilyCard";
 
 const page: React.FC = () => {
+const hasUserData=useRecoilValue(hasUserDataState)
+
   return (
     <Grid sx={{ width: "100%", height: "100%", overflow: "auto" }}>
-      <FamilyCardAria />
-      <FamilyCardDetailAria />
+      {hasUserData?(
+        <Grid>
+          <FamilyCardAria />
+<FamilyCardDetailAria />
+        </Grid>
+      ):(
+        <FirstFamilyCard />
+      )}
+      
     </Grid>
   );
 };
--- data/MenuData.tsx
+++ data/MenuData.tsx
@@ -10,20 +10,20 @@ export const MenuData = [
   // 家族の情報
   { number: 1, title: "Home", link: "/features/home/top", icon: <HomeIcon /> },
   // スケジュール・やる事リスト
-  { number: 2, title: "Todo", link: "todo", icon: <ListAltIcon /> },
+  { number: 2, title: "Todo", link: "/features/home/todo", icon: <ListAltIcon /> },
   // ライフプラン表
-  { number: 3, title: "Life", link: "life", icon: <FaceIcon /> },
+  { number: 3, title: "Life", link: "/features/home/life", icon: <FaceIcon /> },
   // 所持品の保存
-  { number: 4, title: "Property", link: "property", icon: <LaptopChromebookIcon /> },
+  { number: 4, title: "Property", link: "/features/home/property", icon: <LaptopChromebookIcon /> },
   // メモ（URL・Titleをカテゴリごとに）
-  { number: 5, title: "Memo", link: "memo", icon: <AutoAwesomeMotionIcon /> },
+  { number: 5, title: "Memo", link: "/features/home/memo", icon: <AutoAwesomeMotionIcon /> },
   // アカウント情報
-  { number: 6, title: "Account", link: "account", icon: <ManageAccountsIcon /> },
+  { number: 6, title: "Account", link: "/features/home/account", icon: <ManageAccountsIcon /> },
   // このアプリの説明書
   {
     number: 7,
     title: "Instructions",
-    link: "instructions",
+    link: "/features/home/instructions",
     icon: <AutoStoriesIcon />,
   },
 ];
