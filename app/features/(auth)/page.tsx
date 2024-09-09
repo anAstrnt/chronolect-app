35ca887286e184983bc7130f192dadf159ca15ff
an <shinohara.2516300@gmail.com>
2024-09-09 19:53:57 +0900

    ⚡️ Top画面で表示するビジュアルの変更

--- app/features/(auth)/page.tsx
+++ app/features/(auth)/page.tsx
@@ -2,12 +2,21 @@
 
 import React, { useEffect } from "react";
 import Top from "@/app/features/(auth)/top/page";
-import { Grid } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
 import { collection, onSnapshot } from "firebase/firestore";
 import { db } from "@/libs/firebase";
 import FirstFamilyCard from "@/app/components/top/FamilyCardAria/FirstFamilyCard";
 import { hasUserDataState } from "@/app/states/hasUserDataState";
 import { useRecoilState } from "recoil";
+import { MenuData } from "@/data/MenuData";
+import SignOut from "@/app/components/SignOut";
 
 const Page = () => {
   // Firestore/"familyCard"のクエリスナップショットに値が入っていたらTrue。入っていなかったらFalseを返し、ユーザーが初めてアクセスした場合に、表示する画面を切り替えられるようにしている。
@@ -27,8 +36,32 @@ const Page = () => {
   }, []);
 
   return (
-    <Grid sx={{ width: "100%", height: "100%", overflow: "auto" }}>
-      {hasUserData ? <Top /> : <FirstFamilyCard />}
+    // <Grid sx={{ width: "100%", height: "100%", overflow: "auto" }}>
+    //   {hasUserData ? <Top /> : <FirstFamilyCard />}
+    // </Grid>
+    // TODO: TOP画面を変える
+    <Grid>
+      <SignOut />
+      <Grid sx={{ display: "flex" }}>
+        {MenuData.map((data) => (
+          <Card sx={{ maxWidth: 345, margin: "10px", height: "300px" }}>
+            <CardActionArea
+              href={data.link}
+              sx={{ height: "300px", display: "flex" }}
+            >
+              {React.cloneElement(data.icon, { sx: { fontSize: 100, mb: 2 } })}
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {data.title}
+                </Typography>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  {data.detail}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        ))}
+      </Grid>
     </Grid>
   );
 };
--- data/MenuData.tsx
+++ data/MenuData.tsx
@@ -8,24 +8,40 @@ import AutoStoriesIcon from "@mui/icons-material/AutoStories";
 
 export const MenuData = [
   // 家族の情報
-  { number: 1, title: "Home", link: "/features/top", icon: <HomeIcon /> },
-  // スケジュール・やる事リスト
-  { number: 2, title: "Todo", link: "/features/todo", icon: <ListAltIcon /> },
-  // ライフプラン表
-  { number: 3, title: "Life", link: "/features/life", icon: <FaceIcon /> },
-  // 所持品の保存
   {
-    number: 4,
-    title: "Property",
-    link: "/features/property",
-    icon: <LaptopChromebookIcon />,
+    number: 1,
+    title: "FamilyCard",
+    link: "/features/top",
+    icon: <HomeIcon />,
+    detail:
+      "家族のプロフィールや連絡先をひとまとめにして、簡単に管理・共有できるアプリです。大切な家族の情報をすぐに確認でき、スムーズなコミュニケーションをお手伝いします。",
   },
+  // スケジュール・やる事リスト
+  {
+    number: 2,
+    title: "Todo",
+    link: "/features/todo",
+    icon: <ListAltIcon />,
+    detail:
+      "日々のタスクを簡単に整理・管理できるアプリです。やるべきことをリスト化して、優先順位をつけながら効率的に進められるので、忙しい毎日でもスムーズにタスクをこなせます。",
+  },
+  // ライフプラン表
+  // { number: 3, title: "Life", link: "/features/life", icon: <FaceIcon /> },
+  // 所持品の保存
+  // {
+  //   number: 4,
+  //   title: "Property",
+  //   link: "/features/property",
+  //   icon: <LaptopChromebookIcon />,
+  // },
   // メモ（URL・Titleをカテゴリごとに）
   {
     number: 5,
     title: "Memo",
     link: "/features/memo",
     icon: <AutoAwesomeMotionIcon />,
+    detail:
+      "URLとメモを一緒に保存できる便利なアプリです。学習や料理、趣味など、生活に役立つページをメモ付きで整理し、わかりやすく一覧にして管理できます。",
   },
   // アカウント情報
   {
@@ -33,12 +49,14 @@ export const MenuData = [
     title: "Account",
     link: "/features/account",
     icon: <ManageAccountsIcon />,
+    detail:
+      "ユーザーのプロフィールやログイン情報の確認・編集が簡単に行えます。パスワードの変更や通知設定の更新など、アカウントに関する設定をスムーズに管理できます。",
   },
   // このアプリの説明書
-  {
-    number: 7,
-    title: "Instructions",
-    link: "/features/instructions",
-    icon: <AutoStoriesIcon />,
-  },
+  // {
+  //   number: 7,
+  //   title: "Instructions",
+  //   link: "/features/instructions",
+  //   icon: <AutoStoriesIcon />,
+  // },
 ];
