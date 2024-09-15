c6995682dfd427a5befbe0a65ea39f537acf1238
an <shinohara.2516300@gmail.com>
2024-09-15 21:05:19 +0900

    💡 認証機能のコメント追加

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -9,12 +9,16 @@ type useAuthProps = {
   authenticated: boolean;
 };
 
+// アプリ内でグローバルに認証状態管理をするためのフック
 const useAuth = (): useAuthProps => {
   const router = useRouter();
+  // ローディングの状態管理をするステート（初期状態：ローディング中で未認証）
   const [loading, setLoading] = useState<boolean>(true);
+  // ユーザーの状態管理をするステート
   const [authenticated, setAuthenticated] = useState<boolean>(false);
 
   useEffect(() => {
+    // ユーザーの認証状態をリアルタイムで取ってくる処理
     const unsubScribe = auth.onAuthStateChanged((user) => {
       if (user) {
         setAuthenticated(true);
