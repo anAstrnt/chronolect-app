96a390912f5e1f365f9d57f4bfd8eae2d1bb6086
an <shinohara.2516300@gmail.com>
2024-09-15 21:18:46 +0900

    🔥 timeDesplayに関するファイルやコードを削除

--- app/features/(auth)/layout.tsx
+++ app/features/(auth)/layout.tsx
@@ -1,19 +1,7 @@
 "use client";
 
 import React, { ReactNode } from "react";
-import { MenuData } from "@/data/MenuData";
-import {
-  Box,
-  Grid,
-  ImageListItem,
-  List,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-} from "@mui/material";
-import TimeDisplay from "@/app/components/TimeDisplay";
-import SignOut from "@/app/components/SignOut";
-import Link from "next/link";
+import { Grid } from "@mui/material";
 import { RecoilRoot } from "recoil";
 import AuthCheck from "@/app/components/AuthCheck";
 
@@ -23,75 +11,6 @@ type childrenProps = {
 
 const layout = ({ children }: childrenProps) => {
   return (
-    // <Grid
-    //   sx={{
-    //     display: "flex",
-    //     alignItems: "center",
-    //     width: "100%",
-    //     height: "100%",
-    //   }}
-    // >
-    //   <Grid
-    //     sx={{
-    //       display: "flex",
-    //       flexDirection: "column",
-    //       alignItems: "flex-start",
-    //       position: "fixed",
-    //       top: "30px",
-    //       left: "20px",
-    //     }}
-    //   >
-    //     <Box
-    //       sx={{
-    //         display: "flex",
-    //         flexDirection: "column",
-    //         alignItems: "center",
-    //         boxShadow: 3,
-    //         width: "100%",
-    //       }}
-    //     >
-    //       <ImageListItem
-    //         sx={{ padding: 0, width: 200, height: 200, borderRadius: "30px" }}
-    //       >
-    //         <img src="/images/titleLogo.png" />
-    //       </ImageListItem>
-    //       <Box sx={{ borderRight: "grey", padding: 0 }}>
-    //         {MenuData.map((menu) => (
-    //           <List
-    //             key={menu.number}
-    //             sx={{
-    //               width: "100%",
-    //               maxWidth: 200,
-    //               bgcolor: "background.paper",
-    //               padding: 0,
-    //               marginTop: "10px",
-    //               marginBottom: "10px",
-    //             }}
-    //             component="nav"
-    //             aria-labelledby="nested-list-subheader"
-    //           >
-    //             <ListItemButton>
-    //               <Link href={menu.link}>
-    //                 <ListItemIcon>{menu.icon}</ListItemIcon>
-    //                 <ListItemText primary={menu.title} />
-    //               </Link>
-    //             </ListItemButton>
-    //           </List>
-    //         ))}
-    //         <SignOut />
-    //       </Box>
-    //     </Box>
-    //     <TimeDisplay />
-    //   </Grid>
-    //   <Grid
-    //     sx={{
-    //       position: "fixed",
-    //       top: "30px",
-    //       left: "250px",
-    //       width: "100%",
-    //       height: "100%",
-    //     }}
-    //   >
     <RecoilRoot>
       <AuthCheck>
         <Grid
@@ -113,8 +32,6 @@ const layout = ({ children }: childrenProps) => {
         </Grid>
       </AuthCheck>
     </RecoilRoot>
-    //   </Grid>
-    // </Grid>
   );
 };
 
