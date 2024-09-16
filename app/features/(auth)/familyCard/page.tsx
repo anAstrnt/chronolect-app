8ea0e319d5468b131cd64c4c960780ed9e55813d
an <shinohara.2516300@gmail.com>
2024-09-16 21:06:33 +0900

    🐛 menu画面でFamilyCardの登録状況を取得していたため、FamilyCardのディレクトリに処理を移動した

--- app/features/(auth)/familyCard/page.tsx
+++ app/features/(auth)/familyCard/page.tsx
@@ -1,17 +1,34 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import FamilyCardAria from "./FamilyCardAria/page";
 import FamilyCardDetailAria from "./FamilyCardDetailAria/page";
 import { Grid } from "@mui/material";
-import { useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { hasUserDataState } from "@/app/states/hasUserDataState";
 import FirstFamilyCard from "@/app/components/familyCard/FamilyCardAria/FamilyCard";
 import BackToPageButton from "@/components/BackToPageButton";
+import { collection, onSnapshot } from "firebase/firestore";
+import { db } from "@/libs/firebase";
 
 const page: React.FC = () => {
-  const hasUserData = useRecoilValue(hasUserDataState);
+  // Firestore/"familyCard"のクエリスナップショットに値が入っていたらTrue。入っていなかったらFalseを返し、ユーザーが初めてアクセスした場合に、表示する画面を切り替えられるようにしている。
+  const [hasUserData, setHasUserData] = useRecoilState(hasUserDataState);
 
+  useEffect(() => {
+    // FamilyCardの初期登録が完了しているか調べる処理。
+    // !snapshot.emptyでFirestoreのクエリスナップショットの結果が空かどうかを判定し、true（空）でないJSXで<Top /> を表示し、falseであれば、FamilyCardの初期登録画面<FirstFamilyCard />に飛ぶようにしている。
+    const unsubscribe = onSnapshot(collection(db, "familyCard"), (snapshot) => {
+      if (!snapshot.empty) {
+        setHasUserData(true);
+      } else {
+        setHasUserData(false);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
+  // TODO：familyCardが登録されていなかったときの処理を、menu画面からfamilyCardに移動してきた。次はUIを整える。
   return (
     <Grid sx={{ width: "100%", height: "100%", overflow: "auto" }}>
       <BackToPageButton />
